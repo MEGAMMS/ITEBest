94e78d3b177bb6721bf087385e2529d3e5f70332
NGPTechWorld <ngpsupp@gmail.com>
2023-12-08 21:25:59 +0300

    baner1

--- src/main/java/app/View/Home.java
+++ src/main/java/app/View/Home.java
@@ -1,4 +1,6 @@
 package main.java.app.View;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JLabel;
@@ -10,10 +12,19 @@ public class Home extends JPanel{
     }
     private void initComponents(){
         Font f30 = new Font("Arial", Font.BOLD, 30);
-        setBounds(60,50,1220,670);
-        JLabel tHome=new JLabel("Home Page");
+        JPanel baner = new JPanel();
+        baner.setBackground(Color.LIGHT_GRAY);
+        
+    
+        setBounds(60, 50, 1220, 670);
+        JLabel tHome = new JLabel("Home Page");
+        tHome.setBounds(10, 0, 300, 50);
+        baner.setBounds(10, 50, 1200, 250);
         tHome.setFont(f30);
+        setLayout(null);
         add(tHome);
+        add(baner);
+        
     }
     public JPanel getP(){
         return this;
