dd97d16bf316aca1d5dd72950fbb2088840674d9
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-08 21:13:28 +0300

    added font controller and fixed the main method

--- src/main/java/app/ITEBest.java
+++ src/main/java/app/ITEBest.java
@@ -1,5 +1,9 @@
 package main.java.app;
 
+import main.java.app.View.Start;
+
 public class ITEBest {
-        
+        public static void main(String[] args){
+            Start.main();
+        }
 }
