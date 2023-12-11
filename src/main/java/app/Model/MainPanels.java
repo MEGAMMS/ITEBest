d570ba55825ed71f5fb550b3285a1cc3c23dfb2e
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-11 10:38:55 +0300

    added one line in MainPanels.java

--- src/main/java/app/Model/MainPanels.java
+++ src/main/java/app/Model/MainPanels.java
@@ -5,7 +5,8 @@ import javax.swing.JPanel;
 
 public class MainPanels {
     static public ArrayList<JPanel> Panels = new ArrayList<JPanel>();
-    static public void addPanel(JPanel panel){
+
+    static public void addPanel(JPanel panel) {
         Panels.add(panel);
     }
 }
