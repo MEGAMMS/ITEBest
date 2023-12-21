c7fb5393689a09d15bf829b644183c939d772949
NGPTechWorld <ngpsupp@gmail.com>
2023-12-21 22:27:27 +0300

    fix errors add show film more add events

--- src/main/java/app/Controller/PanelsController.java
+++ src/main/java/app/Controller/PanelsController.java
@@ -1,19 +1,31 @@
 package main.java.app.Controller;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
-import javax.swing.JPanel;
-
-import main.java.app.Model.MainPanels;
 import main.java.app.View.Home;
-import main.java.app.View.Start;
+import main.java.app.View.Lobby;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import main.java.app.ITEBest;
+import main.java.app.Model.Database;
+import main.java.app.Model.MainPanels;
+import main.java.app.Model.Movie;
+import main.java.app.View.RoundedBorder;
 import main.java.app.View.Ticketadd;
 import main.java.app.View.itemMove;
 
 public class PanelsController {
-    
+    public static Border roundedBorder(int n){
+        return new RoundedBorder(n);
+    }
+
     public static void switchPanels(JPanel panel) {
         for (JPanel p : MainPanels.Panels) {
             p.setVisible(("B" + p.getName()).equals(panel.getName()));
@@ -23,11 +35,38 @@ public class PanelsController {
         }
     }
 
-    public static void addToToolbar(JPanel panel) {
+    public static void addToToolbar(JPanel panel,String nameP) {
+
+        
         MouseListener ms = new MouseListener() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e) {
-                PanelsController.switchPanels(panel);
+                //PanelsController.switchPanels(panel);
+                //Lobby.PHome.setBody(5);
+                
+                /*CardLayout cardLayout = (CardLayout) Lobby.Cardpanel.getLayout();
+                cardLayout.show(Lobby.Cardpanel, nameP);*/
+                switch (nameP) {
+                    case "Home":
+                        Lobby.PHome.setVisible(true);
+                        Lobby.PProfile.setVisible(false);
+                        Lobby.ticketadd.setVisible(false);
+                        break;
+                    case "Profile":
+                     Lobby.PHome.setVisible(false);
+                        Lobby.PProfile.setVisible(true);
+                        Lobby.ticketadd.setVisible(false);
+
+                    break;
+                    case "Close":
+                        Lobby.PHome.setVisible(true);
+                        Lobby.PProfile.setVisible(false);
+                        Lobby.ticketadd.setVisible(false);
+
+                    break;
+                    default:
+                        break;
+                }
             }
 
             @Override
@@ -56,13 +95,29 @@ public class PanelsController {
             }
         };
         panel.addMouseListener(ms);
-    }
-     public static void Book(itemMove panel) {
+        ActionListener al=new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+            
+        };
+       
+    }
+    
+    public static void Book(itemMove panel,JPanel button,JLabel label,Movie n) {
+
+        
         MouseListener ms = new MouseListener() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e) {
-                System.out.println(panel.getX());
+                System.out.println(panel.movie.getTitle());
+                //ITEBest.lobby.showAddTicket(Database.movies.get(0));
+                Lobby.PHome.setVisible(false);
+                Lobby.ticketadd.setVisible(true);
+                Ticketadd.setValues(Lobby.ticketadd, Database.movies.get(Database.movies.indexOf(n)));
             }
 
             @Override
@@ -81,15 +136,16 @@ public class PanelsController {
 
             @Override
             public void mouseEntered(java.awt.event.MouseEvent e) {
-                panel.setBackground(ColoringController.getLightBasicColor());
+                label.setForeground(ColoringController.getBasicColor());
             }
 
             @Override
             public void mouseExited(java.awt.event.MouseEvent e) {
-                panel.setBackground(Color.white);
-
+                label.setForeground(Color.GRAY);
             }
         };
-        panel.addMouseListener(ms);
+        
+        button.addMouseListener(ms);
+        //panel.addMouseListener(ms);
     }
 }
--- src/main/java/app/View/Lobby.java
+++ src/main/java/app/View/Lobby.java
@@ -6,24 +6,48 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+
 import main.java.app.Controller.ColoringController;
 import main.java.app.Controller.FontController;
 import main.java.app.Controller.ImageController;
 import main.java.app.Controller.PanelsController;
+import main.java.app.Model.Database;
 import main.java.app.Model.MainPanels;
+import main.java.app.Model.Movie;
 
 public class Lobby extends JFrame {
-    Home PHome = new Home();
-    Profile PProfile = new Profile();
+    //Panels
+    public static JPanel maiPanel=new JPanel();
+    public static JPanel Top = new JPanel();
+    public static JPanel toolbar = new JPanel();
 
-    Lobby() {
+
+    public static Home PHome=new Home();
+    public static Profile PProfile = new Profile();
+    public static Ticketadd ticketadd=new Ticketadd();
+    public static Singup singup = new Singup();
+    public void showAddTicket(Movie movie){
+        remove(ticketadd);
+        ticketadd=new Ticketadd(movie);
+        add(ticketadd);
+        ticketadd.setVisible(false);
+
+    }
+    public Lobby() {
         initComponents();
     }
 
+    
     private void initComponents() {
-
+        
+   
+       /*  Cardpanel=new JPanel(new CardLayout());
+        Cardpanel.setBounds(60, 50, 1220, 670);
+        Cardpanel.add(h,"Home");
+        Cardpanel.add(PProfile,"Profile");*/
+    
         // -------------Head---------------
-        JPanel Top = new JPanel();
         JLabel title = new JLabel("ITEBest");
         title.setBounds(600, 0, 300, 50);
         Top.setBounds(0, 0, 1280, 50);
@@ -35,7 +59,7 @@ public class Lobby extends JFrame {
         // --------------------------------
 
         // -----------------toolbar----------------
-        JPanel toolbar = new JPanel();
+        
         toolbar.setBounds(0, 0, 60, 1000);
         toolbar.setBackground(ColoringController.getBasicColor());
         toolbar.setLayout(null);
@@ -46,7 +70,7 @@ public class Lobby extends JFrame {
         BProfile.setBounds(0, 70, 60, 100);
         BProfile.add(ImageController.addPhoto("profile.png"), BorderLayout.CENTER);
         BProfile.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(BProfile);
+        PanelsController.addToToolbar(BProfile,"Profile");
         toolbar.add(BProfile);
         //
         JPanel BMovie = new JPanel();
@@ -54,7 +78,7 @@ public class Lobby extends JFrame {
         BMovie.setBounds(0, 170, 60, 100);
         BMovie.add(ImageController.addPhoto("movie.png"), BorderLayout.CENTER);
         BMovie.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(BMovie);
+        PanelsController.addToToolbar(BMovie,"");
         toolbar.add(BMovie);
         //
         JPanel BTrend = new JPanel();
@@ -62,7 +86,7 @@ public class Lobby extends JFrame {
         BTrend.setBounds(0, 270, 60, 100);
         BTrend.add(ImageController.addPhoto("fire.png"), BorderLayout.CENTER);
         BTrend.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(BTrend);
+        PanelsController.addToToolbar(BTrend,"");
         toolbar.add(BTrend);
         //
         JPanel BHome = new JPanel();
@@ -71,7 +95,8 @@ public class Lobby extends JFrame {
         BHome.setBounds(0, 370, 60, 100);
         BHome.add(ImageController.addPhoto("home.png"), BorderLayout.CENTER);
         BHome.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(BHome);
+        PanelsController.addToToolbar(BHome,"Home");
+        PHome.repaint();
         toolbar.add(BHome);
         //
         JPanel Bticket = new JPanel();
@@ -79,7 +104,7 @@ public class Lobby extends JFrame {
         Bticket.setBounds(0, 470, 60, 100);
         Bticket.add(ImageController.addPhoto("ticket.png"), BorderLayout.CENTER);
         Bticket.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(Bticket);
+        PanelsController.addToToolbar(Bticket,"");
         toolbar.add(Bticket);
         //
         JPanel BSettings = new JPanel();
@@ -87,36 +112,54 @@ public class Lobby extends JFrame {
         BSettings.setBounds(0, 570, 60, 100);
         BSettings.add(ImageController.addPhoto("setting.png"), BorderLayout.CENTER);
         BSettings.setBackground(ColoringController.getBasicColor());
-        PanelsController.addToToolbar(BSettings);
+        PanelsController.addToToolbar(BSettings,"");
         toolbar.add(BSettings);
         //
         // -----------------------------
-
+        
+        
         // ----------Properties------------
         setIconImage(ImageController.getITEBestIcon().getImage());
         setSize(1280, 720);
+        maiPanel.setSize(1280, 720);
         setUndecorated(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        Border roundedBorder = new RoundedBorder(10); 
+        maiPanel.setBorder(roundedBorder);
         setLayout(null);
-        MainPanels.addPanel(PHome);
-        MainPanels.addPanel(PProfile);
-        add(PHome);
-        add(PProfile);
-        PHome.setVisible(false);
+        maiPanel.setBorder(roundedBorder);
+        maiPanel.setLayout(null);
+        maiPanel.add(Top);
+        maiPanel.add(toolbar);
+        //MainPanels.addPanel(PHome);
+        //MainPanels.addPanel(PProfile);
+        maiPanel.add(PHome);
+        maiPanel.add(PProfile);
+        maiPanel.add(ticketadd);
+        maiPanel.add(singup);
+        PHome.setVisible(true);
         PProfile.setVisible(false);
-        add(Top);
-        add(toolbar);
+        ticketadd.setVisible(false);
+        add(maiPanel);
+        
         setVisible(true);
         // --------------------------------
     }
-
+    public void reset(){
+        remove(PHome);
+        System.out.println("Done remove");
+        
+    }/* 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Lobby();
+            //new Lobby();
             }
         });
+    }*/
+
+    public void PHome() {
     }
 }
