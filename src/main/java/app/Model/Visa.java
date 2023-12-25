7a52361670b8b99211486a63ecf39385936c8632
Ken <ahmadsoud63@gmail.com>
2023-12-25 09:14:39 -0800

    added class for visa

--- src/main/java/app/Model/Visa.java
+++ src/main/java/app/Model/Visa.java
@@ -1,19 +1,26 @@
 package main.java.app.Model;
 
 public class Visa {
-    private int id;
+    private String id;
     private int networth;
     private String pass;
-    public Visa(int id,int n){
-        this.networth=n;
+    public Visa(String id,String p){
+        this.pass=p;
         this.id = id;
     }
-    public void setpass(int pass) {
+    public void setpass(String pass) {
         this.pass = pass;
     }
 
-    public int getpass() {
+    public String getpass() {
         return this.pass;
+    }
+    public void setID(String id) {
+         this.id = id;
+    }
+
+    public String getID() {
+        return this.id;
     }
     public void setnetworth(int n) {
         this.networth=n;
