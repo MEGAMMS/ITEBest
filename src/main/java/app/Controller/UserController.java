12b401c90c9605ae5449551dd883dff26f345ee0
NGPTechWorld <ngpsupp@gmail.com>
2023-12-25 13:56:43 +0300

    connevt sign up in ui and Database

--- src/main/java/app/Controller/UserController.java
+++ src/main/java/app/Controller/UserController.java
@@ -4,25 +4,28 @@ import main.java.app.Model.Database;
 import main.java.app.Model.User;
 
 public class UserController {
-    public String AddUser(String name, String email, String password) {
-        if(name.equals(" ")||password.equals(" ")||email.equals(" ")){
+    public static String AddUser(String name, String email, String password) {
+        if(name.equals("")||password.equals("")||email.equals("")){
             return "one of the textboxes is blank";
         }
         if (name.length() > 50||name.length()<2) {
             return "error name is invaled";
         }
         if (password.length() < 8 || password.length() > 50) {
-            return "\\ERROR\\ password is invaled";
+            return "password is invaled";
+        }
+        if (email.length() > 50||email.length()<12) {
+            return "error email is invaled";
         }
         for (User e : Database.users) {
             if (e.getEmail().equals(email)) {
                 return "You already have an account";
             }
         }
-        if (!email.substring(email.length() - 10).equals("@gmail")) {
+        if (!email.substring(email.length() - 10).equals("@gmail.com")) {
             return "that's not an email";
         }
-        Database.users.add(new User(Database.users.size(), name, email, password));
+        //Database.users.add(new User(Database.users.size(), name, email, password));
         return "Signed up successfully";
     }
     
