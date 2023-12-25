fde6879ef79b962645bf111fce5dc65e33372572
NGPTechWorld <ngpsupp@gmail.com>
2023-12-26 00:55:40 +0300

    connect user acount in profile

--- src/main/java/app/Model/Database.java
+++ src/main/java/app/Model/Database.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import main.java.app.ITEBest;
 import main.java.app.Controller.ObjectIO;
 
 public class Database implements Serializable {
@@ -12,7 +13,7 @@ public class Database implements Serializable {
     private static final long serialVersionUID = 6529685098267757690L;
     public static ArrayList<User> users = new ArrayList<User>();
     public static ArrayList<Movie> movies = new ArrayList<Movie>();
-
+    public static User thisUser;
     
     static {
         initDatabase();
@@ -21,7 +22,7 @@ public class Database implements Serializable {
     private static void initDatabase(){
         users = new ArrayList<User>();
         movies = new ArrayList<Movie>();
-
+        thisUser=null;
     }
     public static void save() {
 
@@ -29,6 +30,8 @@ public class Database implements Serializable {
             new File(path).mkdirs();
             ObjectIO.WriteObjectToFile(users, path + "users.dat");
             ObjectIO.WriteObjectToFile(movies, path + "movies.dat");
+            // if(ITEBest.LoginState.equals("Logined"))
+            // ObjectIO.WriteObjectToFile(thisUser, path + "thisUser.dat");
         } catch (IOException ex) {
             System.err.println("Faild to save.");
         }
@@ -39,6 +42,8 @@ public class Database implements Serializable {
         try {
             users = (ArrayList<User>) ObjectIO.LoudObjectFromFile(path + "users.dat");
             movies = (ArrayList<Movie>) ObjectIO.LoudObjectFromFile(path + "movies.dat");
+            // if(ITEBest.LoginState.equals("Logined"))
+            // thisUser=(User)ObjectIO.LoudObjectFromFile(path + "thisUser.dat");
         } catch (ClassNotFoundException | IOException e) {
             System.out.println("Failed to load.");
         }
