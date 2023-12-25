d6dbb37f3fe9ab1afd68538793c3085e2fce895f
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-25 21:09:23 +0300

    completed MoviesListView

---
+++ src/main/java/app/Model/Tags.java
@@ -0,0 +1,10 @@
+package main.java.app.Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Tags {
+    public static ArrayList<String> tags = new ArrayList<>(Arrays.asList(
+            "All", "Action", "Drama", "Science fiction", "Fantasy", "Horror", "Animation", "Thriller"
+        ));
+}
--- src/main/java/app/View/MainFrame.java
+++ src/main/java/app/View/MainFrame.java
@@ -82,16 +82,4 @@ public class MainFrame extends JFrame {
         setVisible(true);
         // --------------------------------
     }
-    /*
-     * public static void main(String[] args) {
-     * SwingUtilities.invokeLater(new Runnable() {
-     * 
-     * @Override
-     * public void run() {
-     * //new Lobby();
-     * }
-     * });
-     * }
-     */
-
 }
