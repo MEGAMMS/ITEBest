7530f4fcef178777256173504d44d8638d26650e
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-26 15:03:02 +0300

    fixed Database

--- src/main/java/app/ITEBest.java
+++ src/main/java/app/ITEBest.java
@@ -8,7 +8,7 @@ import main.java.app.View.MainFrame;
 public class ITEBest {
     public static MainFrame mainFrame;
     public static String LoginState;
-    
+
     public static void main(String[] args) throws InterruptedException {
         // new AddMoviesToDatabase();
         LoginState = "none";
