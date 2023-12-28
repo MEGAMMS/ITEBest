a03d90784675d9d56d76c737d1802e2f6d088b4b
NGPTechWorld <ngpsupp@gmail.com>
2023-12-28 09:59:24 +0300

    uncomplate animation for toolbar

--- src/main/java/app/Controller/LabelController.java
+++ src/main/java/app/Controller/LabelController.java
@@ -1,13 +1,15 @@
 package main.java.app.Controller;
 
+import java.awt.Font;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-import com.kitfox.svg.Font;
 import com.kitfox.svg.Style;
 
+
 public class LabelController {
-    public static JLabel addLabel(String text,java.awt.Font font){
+    public static JLabel addLabel(String text,Font font){
         JLabel label=new JLabel(text);
         label.setFont(font);
         label.setHorizontalAlignment(SwingConstants.CENTER);
