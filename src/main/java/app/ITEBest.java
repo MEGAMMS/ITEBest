b5d5548be97cd820c832f4268b762fa6b6e0ab27
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-30 09:57:20 +0300

    updated .gitignore

--- src/main/java/app/ITEBest.java
+++ src/main/java/app/ITEBest.java
@@ -12,7 +12,7 @@ public class ITEBest {
     public static MainFrame mainFrame;
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        //new DefaultDatabase();
+        new DefaultDatabase();
         System.out.println(Utils.isLogedIn());
         Database.visas.add(new Visa("1111-2222-3333-4444", "1234",15000000));
         Database.movies.get(0).setPrice(1000);
