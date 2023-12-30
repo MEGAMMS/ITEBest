17e13b4e1832444e8ba4f7d87241dd7da1952d1f
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-30 11:59:44 +0300

    color changes

--- src/main/java/app/Controller/ColoringController.java
+++ src/main/java/app/Controller/ColoringController.java
@@ -1,63 +1,72 @@
 package main.java.app.Controller;
+
 import java.awt.Color;
 
 public class ColoringController {
-    public static Color getFirstColor(){
+    public static Color getFirstColor() {
         return Color.decode("#AF2655");
     }
-    public static Color getFirstColorLight1(){
+
+    public static Color getFirstColorLight() {
         return Color.decode("#D17292");
     }
-    public static Color getFirstColorLight2(){
-        return Color.decode("#FBA1B7");
-    }
-    public static Color getFirstColorDark1(){
-        return Color.decode("#D30A35");
-    }
-    public static Color getFirstColorDark2(){
+
+    public static Color getFirstColorDark() {
         return Color.decode("#860A35");
     }
-    //------------------------------------
-    public static Color getSecoundColor(){
+
+    // ------------------------------------
+    public static Color getSecoundColor() {
         return Color.decode("#FDF7E4");
     }
-    public static Color getSecoundColorLight(){
+
+    public static Color getSecoundColorLight() {
         return Color.decode("#EBE3D5");
     }
-    public static Color getSecoundColorDark1(){
+
+    public static Color getSecoundColorDark1() {
         return Color.decode("#EAD7BB");
     }
-    public static Color getSecoundColorDark2(){
+
+    public static Color getSecoundColorDark2() {
         return Color.decode("#776B5D");
     }
-    public static Color getSecoundColorDarkLight1(){
+
+    public static Color getSecoundColorDarkLight1() {
         return Color.decode("#B0A4A4");
     }
-    public static Color getSecoundColorDarkLight2(){
+
+    public static Color getSecoundColorDarkLight2() {
         return Color.decode("#F3EEEA");
     }
-    //------------------------------------
-    
-    public static Color getWhiteColor(){
+    // ------------------------------------
+
+    public static Color getWhiteColor() {
         return Color.white;
-        
+
     }
-    public static Color getLightGrayColor(){
+
+    public static Color getLightGrayColor() {
         return Color.decode("#EEEEEE");
     }
-    public static Color getDarkGray2Color(){
+
+    public static Color getDarkGray2Color() {
         return Color.decode("#C9C9C9");
     }
-    public static Color getBlackColor(){
+
+    public static Color getBlackColor() {
         return Color.BLACK;
     }
-    public static Color getRedColor(){
+
+    public static Color getRedColor() {
         return Color.decode("#B80000");
     }
-    public static Color getGreenColor(){
+
+    public static Color getGreenColor() {
         return Color.decode("#1C6758");
     }
-    public static Color getVisaColor(){
+
+    public static Color getVisaColor() {
         return Color.decode("#3887BE");
     }
 }
