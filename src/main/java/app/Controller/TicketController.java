556ab49eac8a4b42b535d10d5c2d887a03a44847
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-30 16:24:25 +0300

    withdraw done

--- src/main/java/app/Controller/TicketController.java
+++ src/main/java/app/Controller/TicketController.java
@@ -9,28 +9,28 @@ import main.java.app.Model.Visa;
 
 public class TicketController {
     public static String Book(Movie movie, User user, int count, Showtime showtime) {
-        if (count == 0 || showtime ==null) {
+        if (count == 0 || showtime == null) {
             return "please fill all the required information";
         }
-        if(user.creditcard == null){
+        if (user.creditcard == null) {
             return "You don't have a creditcard";
         }
         if (movie.getPrice() * count > user.creditcard.getNetworth()) {
             return "Unfortunately you can not afford this transaction";
         }
-        if(showtime.seats < count){
+        if (showtime.seats < count) {
             return "Sorry, not enough seats";
         }
         showtime.seats -= count;
         for (int i = 0; i < count; i++) {
             user.creditcard.setNetworth(user.creditcard.getNetworth() - movie.getPrice());
-            user.tickets.add(new Ticket(user.tickets.size(), movie, user, showtime,showtime.seats+i+1));
+            user.tickets.add(new Ticket(user.tickets.size(), movie, user, showtime, showtime.seats + i + 1));
         }
         Database.save();
         return "Booked successfully";
     }
 
-    public String UnBook(Ticket ticket) {
+    public static String UnBook(Ticket ticket) {
         User user = ticket.getUser();
         Visa visa = user.creditcard;
         Movie movie = ticket.getMovie();
@@ -38,6 +38,7 @@ public class TicketController {
         visa.setNetworth(visa.getNetworth() + movie.getPrice());
         showtime.seats++;
         user.tickets.remove(ticket);
+        for(int i = 0;i <  ticket.getUser().tickets.size();i++)ticket.getUser().tickets.get(i).setId(i);
         return "Ticket withdrawn";
     }
 
