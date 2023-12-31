34595f92d06dc2bf02865021dd176943860ef345
Mouaaz Sweid <mmsweid@gmail.com>
2023-12-31 20:11:54 +0300

    small change

--- src/main/java/app/Controller/DefaultDatabase/AddCinemas.java
+++ src/main/java/app/Controller/DefaultDatabase/AddCinemas.java
@@ -16,9 +16,9 @@ public class AddCinemas {
                 new ArrayList<Movie>(Database.movies.subList(10, Database.movies.size())), 90));
         for (int i = 0; i < 5; i++)
             Database.movies.get(i).setCinema(Database.halls.get(0));
-        for (int i = 10; i < Database.movies.size(); i++)
-            Database.movies.get(i).setCinema(Database.halls.get(0));
         for (int i = 5; i < 10; i++)
+            Database.movies.get(i).setCinema(Database.halls.get(0));
+        for (int i = 10; i < Database.movies.size(); i++)
             Database.movies.get(i).setCinema(Database.halls.get(0));
     }
 }
